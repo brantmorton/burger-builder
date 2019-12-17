bef6d824a8264af8db43cc74da0ca8f61bd2f009
brantmorton <brantmort@gmail.com>
2019-12-16 19:52:34 -0600

    added price summary to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,6 +20,9 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        Total Price: <strong>${props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>
         CANCEL
