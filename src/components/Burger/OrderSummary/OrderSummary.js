6f8c13f86b141ae3a1459f85936ca8d8625c03a1
brantmorton <brantmort@gmail.com>
2019-12-20 19:50:20 -0600

    moved Layout dir to hoc dir

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Aux from "../../../hoc/Auxilary";
+import Aux from "../../../hoc/Auxillary/Auxilary";
 import Button from "../../UI/Button/Button";
 
 const orderSummary = props => {
